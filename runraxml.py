5f5a73a0a4e1c97cfabe7ed8db094308c98e92f5
Kevin Gori <kgori@ebi-005.ebi.ac.uk>
2012-02-20 22:11:40 +0000

    experimental branch first commit

--- runraxml.py
+++ runraxml.py
@@ -1,26 +1,21 @@
 #!/usr/bin/env python
 
-import glob,os,sys
+import glob,os,shlex,subprocess,sys
 from raxml_wrapper import run_RAxML
 from handleArgs import handleArgs
 
-args = handleArgs(sys.argv,help='''
+command_line_args = handleArgs(sys.argv,help='''
 runraxml.py arguments:
          -l = Location, either 'cluster' or 'local' (default) 
 ''')
 
-besttree_path = "./trees/besttrees"
-info_path = "./trees/info"
-if not os.path.exists(besttree_path): os.mkdir(besttree_path)
-if not os.path.exists(info_path): os.mkdir(info_path)
-
 MSAs = glob.glob('./MSA/*.phy')
-for alignment in MSAs:
+for alignment in MSAs[0:1]:
     name = 'gene'+alignment[alignment.index('_')+1:alignment.rindex('.')]
     print alignment, name
-    if args['-l']=="cluster":
-        run_RAxML('raxml', alignment, 'PROTGAMMAWAG', name, nruns=10, 
-    random_tree=True, pthreads=True, nthreads=8, bsub=True)
-    elif args['-l']=="local":
-        run_RAxML('raxml', alignment, 'PROTGAMMAWAG', name, nruns=1, 
-    random_tree=False, pthreads=False, bsub=False)
+    if command_line_args['-l']=="cluster":
+        process_args = 'bsub -o /dev/null -q research-rh6 "raxml -T 4 -m PROTGAMMAWAG -s {0} -n {1} -N 10 && mv RAxML_bestTree.{1} ./trees/besttrees/{1}.nwk && mv RAxML_info.{1} ./trees/info/{1}.info && rm RAxML* "'.format(alignment, name)
+        subprocess.call( shlex.split(process_args) )
+    elif command_line_args['-l']=="local":
+        process_args = 'raxml -T 4 -m PROTGAMMAWAG -s {0} -n {1} && mv RAxML_bestTree.{1} ./trees/besttrees/{1}.nwk && mv RAxML_info.{1} ./trees/info/{1}.info && rm RAxML* '.format(alignment,name)
+        subprocess.call( shlex.split(process_args) )
