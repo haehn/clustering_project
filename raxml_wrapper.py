4e53d9af5f7723a196b619b349461c671ef89b48
Kevin Gori <kgori@ebi-005.ebi.ac.uk>
2012-02-20 16:58:46 +0000

    raxml wrapper works on cluster

--- raxml_wrapper.py
+++ raxml_wrapper.py
@@ -3,12 +3,12 @@
 import glob,os
 
 def run_RAxML(executable, sequence_file, model, run_name, nruns=1, random_tree=False, pthreads = False, bsub=False, nthreads = 2):
-    import subprocess
+    import subprocess, shlex
     if bsub: executable = 'bsub -o /dev/null {0}'.format(executable)
-    if pthreads: args = (executable, '-T', str(nthreads), '-s', sequence_file, '-m', model, '-n', run_name, '-N', str(nruns))
-    else: args = (executable, '-s', sequence_file, '-m', model, '-n', run_name, '-N', nruns)
-    if random_tree: args = args + ('-d',)
-    subprocess.call(args)
+    if pthreads: args = "{0} -T {1} -s {2} -m {3} -n {4} -N {5}".format(executable, str(nthreads), sequence_file, model, run_name, str(nruns))
+    else: args = args = "{0} -s {1} -m {2} -n {3} -N {4}".format(executable, sequence_file, model, run_name, str(nruns))
+    if random_tree: args = args + ' -d'
+    subprocess.call(shlex.split(args))
 
 besttree_path = "./trees/besttrees"
 info_path = "./trees/info"
@@ -16,9 +16,8 @@ if not os.path.exists(besttree_path): os.mkdir(besttree_path)
 if not os.path.exists(info_path): os.mkdir(info_path)
 
 MSAs = glob.glob('./MSA/*.phy')
-for alignment in MSAs[0:1]:
+for alignment in MSAs:
     name = 'gene'+alignment[alignment.index('_')+1:alignment.rindex('.')]
-    run_RAxML('raxml', alignment, 'PROTGAMMAWAG', name, nruns=10, 
+    print alignment, name
+    run_RAxML('raxml', alignment, 'PROTGAMMAWAG', name, nruns=1, 
     random_tree=True, pthreads=True, nthreads=8, bsub=True)
-    os.rename('./RAxML_bestTree.{0}'.format(name), './trees/besttrees/bestTree.{0}'.format(name))
-    os.rename('./RAxML_info.{0}'.format(name), './trees/info/info.{0}'.format(name))
